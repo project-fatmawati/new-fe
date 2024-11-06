5a343a9761e2453bce3931160e1b1e47eb970714
Yana Udin <yn.udin1259@gmail.com>
2024-11-07 04:39:44 +0700

    edit app.jsx, tp masih belum kelar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
 import Katalog from './pages/Katalog'
 import DetailProduk from './pages/DetailProduk'
 import Home from './pages/Home'
@@ -10,26 +10,35 @@ import Homepage from './pages/HomePage'
 import Profile from './pages/Profile'
 import LoginPage from './pages/LoginPage'
 import ChallengeDetail from './pages/ChallengeDetail'
-import RegisterPage from './pages/RegisterPage'
+import RegisterPage from './pages/Register'
+import { AuthProvider } from './context/AuthContext';
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Daftar from "./pages/Register";
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path='/' element = {<Home/>}/>
-      <Route path='/Katalog' element = {<Katalog/>}/>
-      <Route path='/DetailProduk' element = {<DetailProduk/>}/>
-      <Route path='/Cart' element = {<Cart/>}/>
-      <Route path='/Payment' element = {<Payment/>}/>
-      <Route path='/Homepage' element = {<Homepage/>}/>
-      <Route path='/Profile' element = {<Profile/>}/>
-      <Route path='/LoginPage' element = {<LoginPage/>}/>
-      <Route path='/RegisterPage' element = {<RegisterPage/>}/>
-      <Route path='/ChallengeDetail' element = {<ChallengeDetail/>}/>
-    </Routes>
-
-    </>
-  )
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<><Navbar /><LandingPage /><Footer /></>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/daftar" element={<Daftar />} />
+          <Route path='/' element = {<Home/>}/>
+          <Route path='/Katalog' element = {<Katalog/>}/>
+          <Route path='/DetailProduk' element = {<DetailProduk/>}/>
+          <Route path='/Cart' element = {<Cart/>}/>
+          <Route path='/Payment' element = {<Payment/>}/>
+          <Route path='/Homepage' element = {<Homepage/>}/>
+          <Route path='/Profile' element = {<Profile/>}/>
+          <Route path='/LoginPage' element = {<LoginPage/>}/>
+          <Route path='/RegisterPage' element = {<RegisterPage/>}/>
+          <Route path='/ChallengeDetail' element = {<ChallengeDetail/>}/>
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
-
-export default App
+export default App;
