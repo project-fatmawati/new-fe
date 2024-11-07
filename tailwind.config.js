fbbff354fb3db0db5a950970ff11c7e6118cfa2f
Yana Udin <yn.udin1259@gmail.com>
2024-11-07 17:29:42 +0700

    add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
+  content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#F5F5F5',
+      'cyan': '#48CFCB',
+      'teal': '#229799',
+      'slate-blue': '#427898',
+      'mustard': '#F5BD41',
+      'black': '#000000',
+      'red' : '#FF0707',
+    },
   },
   plugins: [],
 }
+
