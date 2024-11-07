fc41f6238ebaf3567b04b5b4e3be5d59622bdca3
Rika Kamila <rikakamila1717@gmail.com>
2024-11-07 14:29:14 +0700

    there's router problem

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,8 @@ import Cart from './pages/Cart'
 import Payment from './pages/Payment';
 import Homepage from './pages/HomePage'
 import Profile from './pages/Profile'
-// import LoginPage from './pages/LoginPage'
 import ChallengeDetail from './pages/ChallengeDetail'
-// import RegisterPage from './pages/Register'
 // import { AuthProvider } from './context/AuthContext';
-// import Navbar from "./component/Navbar";
 // import Footer from "./component/Footer";
 // import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
@@ -21,24 +18,25 @@ import Daftar from "./pages/Register";
 function App() {
   return (
     // <AuthProvider>
-      // <Router>
+
+      <Router>
         <Routes>
           {/* <Route path="/" element={<><Navbar /><LandingPage /><Footer /></>} /> */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/daftar" element={<Daftar />} />
-          <Route path='/' element = {<Home/>}/>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Daftar" element={<Daftar />} />
+          <Route path='/Home' element = {<Home/>}/>
           <Route path='/Katalog' element = {<Katalog/>}/>
           <Route path='/DetailProduk' element = {<DetailProduk/>}/>
           <Route path='/Cart' element = {<Cart/>}/>
           <Route path='/Payment' element = {<Payment/>}/>
-          <Route path='/Homepage' element = {<Homepage/>}/>
+          <Route path='/' element = {<Homepage/>}/>
           <Route path='/Profile' element = {<Profile/>}/>
-          {/* <Route path='/LoginPage' element = {<LoginPage/>}/>
-          <Route path='/RegisterPage' element = {<RegisterPage/>}/> */}
           <Route path='/ChallengeDetail' element = {<ChallengeDetail/>}/>
         </Routes>
-      // </Router>
-    // {/* </AuthProvider> */}
+      </Router>
+
+      // </AuthProvider>
+
   )
 }
 export default App;
